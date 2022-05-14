0173be14fdd23ef98d0379cb08d8b32f567e307c
StefanieNeumann00 <stefanie.neumann.ka@outlook.de>
2022-05-14 13:34:40 +0200

    Remove dead code

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -32,14 +32,16 @@ class Customer
 
         for (Rental rental: rentals)
         {
-            double thisAmount = 0;;
+            double thisAmount = 0;
             //determine amounts for each line
             thisAmount = amountFor(rental);
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
             if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
-                frequentRenterPoints ++;
+            {
+                frequentRenterPoints++;
+            }
             //show figures for this rental
             result += "\t" + rental.getMovie().getTitle()+ "\t" + "\t" + rental.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
